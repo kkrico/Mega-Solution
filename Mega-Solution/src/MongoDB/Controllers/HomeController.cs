d41a7c95d1b533ea85ff52644b10a22b8d4f1bc0
Luan Caius <luancaius@outlook.com>
2017-06-05 20:43:31 -0300

    A lot of bugs because there is a reference problem with mongodriver and the project.json.

--- Mega-Solution/src/MongoDB/Controllers/HomeController.cs
+++ Mega-Solution/src/MongoDB/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using MongoDB.Driver;
 
 namespace MongoDB.Controllers
 {
@@ -10,22 +11,11 @@ namespace MongoDB.Controllers
     {
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
+            var client = new MongoClient();
 
             return View();
         }
 
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
 
         public IActionResult Error()
         {
